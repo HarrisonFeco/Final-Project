7ade56ee12eefc468a422c5c529e941a0af1e29d
LuisH004 <luhaby7@gmail.com>
2024-12-03 19:20:19 -0600

    s
    s

--- Final-Project/Assets/Ethans Assets/_Scripts/TimingLoop.cs
+++ Final-Project/Assets/Ethans Assets/_Scripts/TimingLoop.cs
@@ -1,7 +1,6 @@
 using System;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class TimingLoop : MonoBehaviour
@@ -17,62 +16,55 @@ public class TimingLoop : MonoBehaviour
     [Header("Dynamic")]
     [SerializeField] private GameObject curCheckPt;
     [SerializeField] private GameObject prevCheckPoint;
-    [SerializeField] private int curLap = 0; 
+    [SerializeField] private int curLap = 0;
     [SerializeField] private float startTime;
-    [SerializeField] private float endTime;
     [SerializeField] private float curTime;
     [SerializeField] private int numCPPassed = 0;
-    [SerializeField] private float bestTime;  
+    [SerializeField] private float bestTime;
 
     [Tooltip("Check box to reset best lap time.")]
     public bool resetBestLapTime = false;
 
     [Header("Reset")]
-    public GameObject playerBike; 
-    private Vector3 lastCheckpointPosition; // Store the position of the last checkpoint
-    private Vector3 lastCheckpointRotation;
+    public GameObject playerBike;
+    private Vector3 lastCheckpointPosition;
+    private Quaternion lastCheckpointRotation;
 
     void Start()
     {
-        if (PlayerPrefs.HasKey("BestTime"))
-        {
-            bestTime = PlayerPrefs.GetFloat("BestTime"); 
-        }
-
-        UITxtBestTime.text = "Best Time:   " + Math.Round(bestTime, 3);
-        UITxtLap.text = "Lap " + curLap + " of " + totalLaps;
+        bestTime = PlayerPrefs.GetFloat("BestTime", 99999.999f);
+        UITxtBestTime.text = $"Best Time:   {Math.Round(bestTime, 3)}";
+        UITxtLap.text = $"Lap {curLap} of {totalLaps}";
         lastCheckpointPosition = SFLine.transform.position;
-        lastCheckpointRotation = SFLine.transform.rotation;////////
+        lastCheckpointRotation = SFLine.transform.rotation;
     }
 
     void Update()
     {
         curTime += Time.deltaTime;
-        UITxtTime.text = "Current Time:   " + Math.Round(curTime, 3);
-        if (Input.GetKeyDown(KeyCode.R))      // Check if "R" key is pressed to reset to last checkpoint
+        UITxtTime.text = $"Current Time:   {Math.Round(curTime, 3)}";
+        if (Input.GetKeyDown(KeyCode.R))
         {
             ResetToLastCheckpoint();
         }
     }
 
     void ChangeBestTime(float timeCheck)
-    { // compared new time vs best lap time
-        if (timeCheck < PlayerPrefs.GetFloat("BestTime"))
-        { // if new time is better, save to PlayerPrefs
-            PlayerPrefs.SetFloat("BestTime", timeCheck);
+    {
+        if (timeCheck < bestTime)
+        {
+            bestTime = timeCheck;
+            PlayerPrefs.SetFloat("BestTime", bestTime);
+            UITxtBestTime.text = $"Best Time:   {Math.Round(bestTime, 3)}";
         }
-
-        bestTime = PlayerPrefs.GetFloat("BestTime");
-        UITxtBestTime.text = "Best Time:   " + Math.Round(bestTime, 3);
     }
 
     void OnTriggerEnter(Collider other)
-    { //when the bike passed through a checkpoint it calls this function
+    {
         prevCheckPoint = curCheckPt;
         curCheckPt = other.gameObject;
         LoopCheck();
 
-        // Update the last checkpoint position when crossing a new checkpoint
         if (curCheckPt != SFLine)
         {
             lastCheckpointPosition = curCheckPt.transform.position;
@@ -83,10 +75,9 @@ public class TimingLoop : MonoBehaviour
         {
             Debug.Log("User has finished the race!");
             SceneManager.LoadScene("MainMenu");
-            Debug.Log("Switching to Main Menu scene");
         }
 
-        UITxtLap.text = "Lap " + curLap + " of " + totalLaps;
+        UITxtLap.text = $"Lap {curLap} of {totalLaps}";
     }
 
     void LoopCheck()
@@ -98,20 +89,21 @@ public class TimingLoop : MonoBehaviour
                 Debug.LogWarning("User tried to pass through the same checkpoint twice.");
                 return;
             }
+
             if (numCPPassed >= (CheckPoints.Length - 1) && curCheckPt == SFLine)
             {
-                endTime = Time.time - startTime;
-                // calculates end timing loop at Start-Finish Line after final Checkpoint crossed
+                float endTime = Time.time - startTime;
                 ChangeBestTime(endTime);
-                numCPPassed = 1;
+                numCPPassed = 0; // Reset checkpoint counter
                 curLap++;
             }
             else if (curCheckPt == SFLine)
-            { // starts timing loop at Start-Finish Line
-                numCPPassed = 1;
+            {
+                numCPPassed = 0; // Reset checkpoint counter
                 curTime = 0;
                 startTime = Time.time;
             }
+
             numCPPassed++;
         }
     }
@@ -120,10 +112,10 @@ public class TimingLoop : MonoBehaviour
     {
         if (playerBike != null)
         {
-            // Reset the player's position to the last checkpoint
-            playerBike.transform.position = lastCheckpointPosition;  
-            playerBike.transform.rotation = lastCheckpointRotation; 
-            // Optionally reset the player's velocity or add any other reset logic here
+            playerBike.transform.position = lastCheckpointPosition;
+            Vector3 checkpointForward = lastCheckpointRotation * Vector3.forward;
+            Quaternion correctedRotation = Quaternion.LookRotation(checkpointForward, Vector3.up);
+            playerBike.transform.rotation = correctedRotation;
             Rigidbody rb = playerBike.GetComponent<Rigidbody>();
             if (rb != null)
             {
