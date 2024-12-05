d99761ee7b351a60b2d0bb27c1849d42f1c3730a
Outlaw20205 <155694658+Outlaw20205@users.noreply.github.com>
2024-12-05 14:51:54 -0600

    SetupForTrackSelect

--- Final-Project/Assets/Ethans Assets/_Scripts/SwitchScene.cs
+++ Final-Project/Assets/Ethans Assets/_Scripts/SwitchScene.cs
@@ -14,14 +14,14 @@ public class SwitchScene : MonoBehaviour {
         SceneManager.LoadScene("Settings"); 
         Debug.Log("Switching to Settings scene");
     }
-    public void GameMenuSwitch()
+    public void Track1Switch()
     {
         SceneManager.LoadScene("Test_Track");
-        Debug.Log("Switching to Game scene");
+        Debug.Log("Switching to Test_Track scene");
     }
-    public void HelpSceneSwitch()
+    public void Track2Switch()
     {
-        SceneManager.LoadScene("Help"); 
-        Debug.Log("Opening Help scene");
+        SceneManager.LoadScene("Test_Track2");
+        Debug.Log("Switching to Test_Track2 scene");
     }
 }
